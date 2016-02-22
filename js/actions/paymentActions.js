b9250efc10db823637ad02438ba0ca53b0826c31
David Hsu <dvdhsu@gmail.com>
2016-02-22 14:23:01 +0000

    Promise all update after payment.

--- js/actions/paymentActions.js
+++ js/actions/paymentActions.js
@@ -40,14 +40,17 @@ exports.pay = function pay(email, token, toId, note, amount, navigator) {
   var nonce = "fake-valid-nonce"
 
   return dispatch => {
+    var promiseAll = Promise.all([
+      dispatch(fetchSocialFeed(email, token)),
+      dispatch(fetchPrivateFeed(email, token))
+    ])
+
     dispatch(requestPayment());
     return ajax.pay(email, token, payment, nonce)
     .then(response => {
-      dispatch(fetchSocialFeed(email, token)).then(() => {
-        dispatch(fetchPrivateFeed(email, token)).then(() => {
+      promiseAll.then(() => {
           response.json()
           .then(json => dispatch(receivePayment(json.data, navigator)))
-        })
       });
     })
     .catch(error => console.log(error));
