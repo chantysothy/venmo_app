9a42335b72d050374262569632690df58e439095
Anthony Guo <anthony.guo@some.ox.ac.uk>
2016-03-02 14:37:24 +0000

    Add placeholder android push notification manager

---
+++ js/utils/pushNotificationManager.android.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//var pushNotificationManager = require('NativeModules').PushNotificationManager;
+
+//TODO replace with actual functions.
+exports = module.exports = {}
+exports.getUserId = function() {};
+exports.registerForPushNotifications = function() {};
