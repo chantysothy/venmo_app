073c8239952ceba814e7e8ef243eeae5fdc2dc08
David Hsu <dvdhsu@gmail.com>
2016-03-04 18:40:13 +0000

    Don't ask for push notifications on home screen mount.

--- js/components/home/home.js
+++ js/components/home/home.js
@@ -43,7 +43,7 @@ class Home extends Component {
       });
     });
 
-    PushNotificationManager.registerForPushNotifications();
+    //PushNotificationManager.registerForPushNotifications();
   }
 
   render() {
