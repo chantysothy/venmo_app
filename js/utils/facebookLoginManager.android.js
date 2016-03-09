ad49120ba202903e710d27f761a162908adb1219
Anthony Guo <anthony.guo@some.ox.ac.uk>
2016-03-09 20:12:07 +0000

    Add user friends permission to android fb login

--- js/utils/facebookLoginManager.android.js
+++ js/utils/facebookLoginManager.android.js
@@ -3,7 +3,7 @@
 var {NativeModules} = require('react-native');
 var androidFacebookLoginManager = NativeModules.FBLoginManager;
 
-var PERMISSIONS_TO_INCLUDE = ["email", "public_profile"];
+var PERMISSIONS_TO_INCLUDE = ["email", "public_profile", "user_friends"];
 
 exports = module.exports = {}
 
